4215ae67658737e1662627881c69de10ec3700ba
vivi2019520 <1434867930@qq.com>
2021-05-26 17:18:37 +0800

    修改run_test.py

--- run_test.py
+++ run_test.py
@@ -4,7 +4,5 @@ import pytest
 import allure
 
 if __name__ == '__main__':
-    # 执行pytest单元测试，生成allure报告到allure_report下
-    pytest.main(["-v", "--alluredir=./allure_report"])
-    # 生成测试报告到report目录下
-    os.system("allure generate ./allure_report -o ./report --clean")
+    pytest.main(["-v"])
+
